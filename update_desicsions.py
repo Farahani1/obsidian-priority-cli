833280b13f8dea4650c1349d0d889e95f0c90b42
Hossein <h.sh.farahani1@gmail.com>
2026-06-08 02:25:45 -0700

    desicsion find and updates tasks

--- update_desicsions.py
+++ update_desicsions.py
@@ -1,7 +1,7 @@
-from decision import process_vault
+from decision.priority import process_vault
 
 if __name__ == "__main__":
-    your_vault_path = r"C:\path\to\your\obsidian\vault"   # Windows example
+    your_vault_path = r"./tasks"   # Windows example
     # your_vault_path = "/Users/yourname/vault"           # macOS / Linux example
     
     if "your/obsidian/vault" in your_vault_path or not your_vault_path:
